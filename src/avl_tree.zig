c4be707fcbe54bbb6b68950879289e2f6e4492e6
Sam Foucart <samfoucart25@gmail.com>
2026-04-18 19:41:40 -0400

    removed unecessary printing

--- src/avl_tree.zig
+++ src/avl_tree.zig
@@ -48,39 +48,30 @@ pub fn AvlTree(
             pub fn next(self: *ItrSelf, gpa: Allocator) !?T {
                 if (self.internal_stack) |*internal_stack| {
                     if (internal_stack.size == 0) {
-                        std.debug.print("AvlTree.ForwardIterator.next() - returning null because stack existed and was empty\n", .{});
                         return null;
                     }
                 } else {
                     self.internal_stack = ArrayStack(TraversalRecursionState).empty;
 
-                    std.debug.print("AvlTree.ForwardIterator.next() - Adding root node to stack\n", .{});
                     try self.internal_stack.?.push(gpa, .{ .state = TraversalRecursionStates.first, .node = self.tree.root });
                 }
 
                 if (self.internal_stack) |*internal_stack| {
                     while (internal_stack.size > 0) {
-                        std.debug.print("AvlTree.ForwardIterator.next() - popping new node\n", .{});
                         const current = try internal_stack.pop(gpa);
 
                         if (current.node) |node_nn| {
                             if (current.state == TraversalRecursionStates.first) {
                                 try internal_stack.push(gpa, .{ .state = TraversalRecursionStates.second, .node = current.node });
                                 try internal_stack.push(gpa, .{ .state = TraversalRecursionStates.first, .node = node_nn.left });
-
-                                std.debug.print("AvlTree.ForwardIterator.next() - adding left node to stack\n", .{});
                             } else if (current.state == TraversalRecursionStates.second) {
                                 try internal_stack.push(gpa, .{ .state = TraversalRecursionStates.first, .node = node_nn.right });
-                                std.debug.print("AvlTree.ForwardIterator.next() - adding right node to stack and returning current\n", .{});
 
                                 return node_nn.val;
                             }
-                        } else {
-                            std.debug.print("AvlTree.ForwardIterator.next() - continuing past null node\n", .{});
-                        }
+                        } else {}
                     }
 
-                    std.debug.print("AvlTree.ForwardIterator.next() - exhausted all nodes\n", .{});
                     return null;
                 } else {
                     return error.InvalidInternalState;
@@ -227,15 +218,12 @@ pub fn AvlTree(
 
                         // Right Right Imbalance
                         if (right_balance == 1 or right_balance == 0) {
-                            std.debug.print("AvlTree.fixAvlProperty(self, {d}) - Right Right Imbalance - Rotating Left {d}\n", .{ node_nn.val, node_nn.val });
                             return try self.rotateLeft(node_nn);
                         }
 
                         // Right Left Imabalance
                         if (right_balance == -1) {
-                            std.debug.print("AvlTree.fixAvlProperty(self, {d}) - Right Left Imbalance - Rotating Right {d}\n", .{ node_nn.val, right.val });
                             node_nn.right = try self.rotateRight(right);
-                            std.debug.print("AvlTree.fixAvlProperty(self, {d}) - Right Left Imbalance - Rotating Left {d}\n", .{ node_nn.val, node_nn.val });
                             return try self.rotateLeft(node_nn);
                         }
 
@@ -254,15 +242,12 @@ pub fn AvlTree(
 
                         // Left Right Imbalance
                         if (left_balance == 1) {
-                            std.debug.print("AvlTree.fixAvlProperty(self, {d}) - Left Right Imbalance - Rotating Left {d}\n", .{ node_nn.val, left.val });
                             node_nn.left = try self.rotateLeft(left);
-                            std.debug.print("AvlTree.fixAvlProperty(self, {d}) - Left Right Imbalance - Rotating Right {d}\n", .{ node_nn.val, node_nn.val });
                             return try self.rotateRight(node_nn);
                         }
 
                         // Left Left Imabalance
                         if (left_balance == -1 or left_balance == 0) {
-                            std.debug.print("AvlTree.fixAvlProperty(self, {d}) - Left Left Imbalance - Rotating Right {d}\n", .{ node_nn.val, node_nn.val });
                             return try self.rotateRight(node_nn);
                         }
 
@@ -294,37 +279,29 @@ pub fn AvlTree(
                 var recursion = ArrayStack(RecursionState).empty;
                 defer recursion.deinit(gpa);
 
-                std.debug.print("AvlTree.add(self, gpa, {d}) - adding root to stack\n", .{val});
                 try recursion.push(gpa, .{ .traversal_position = TraversalPosition.initial, .node = self.root });
 
                 var last_touched_node: ?*AvlNode = null;
 
                 while (recursion.size > 0) {
-                    std.debug.print("AvlTree.add(self, gpa, {d}) - popping off of stack\n", .{val});
                     const current = try recursion.pop(gpa);
                     if (current.node) |node| {
                         if (current.traversal_position == TraversalPosition.initial) {
-                            std.debug.print("AvlTree.add(self, gpa, {d}) - popped {d} off initially\n", .{ val, node.val });
                             try recursion.push(gpa, .{ .traversal_position = TraversalPosition.addNode, .node = current.node });
 
                             const comparison = compareFn(val, node.val);
                             if (comparison == std.math.Order.lt or comparison == std.math.Order.eq) {
-                                std.debug.print("AvlTree.add(self, gpa, {d}) - going left from {d}\n", .{ val, node.val });
                                 try recursion.push(gpa, .{ .traversal_position = TraversalPosition.initial, .node = node.left });
                             } else {
-                                std.debug.print("AvlTree.add(self, gpa, {d}) - going right from {d}\n", .{ val, node.val });
                                 try recursion.push(gpa, .{ .traversal_position = TraversalPosition.initial, .node = node.right });
                             }
                         } else if (current.traversal_position == TraversalPosition.addNode) {
-                            std.debug.print("AvlTree.add(self, gpa, {d}) - popped {d} off second time\n", .{ val, node.val });
                             const comparison = compareFn(val, node.val);
 
                             if (comparison == std.math.Order.lt or comparison == std.math.Order.eq) {
-                                std.debug.print("AvlTree.add(self, gpa, {d}) - fixing avl left from {d}\n", .{ val, node.val });
                                 node.left = last_touched_node;
                                 node.left = try self.fixAvlProperty(node.left);
                             } else {
-                                std.debug.print("AvlTree.add(self, gpa, {d}) - fixing avl right from {d}\n", .{ val, node.val });
                                 node.right = last_touched_node;
                                 node.right = try self.fixAvlProperty(node.right);
                             }
@@ -334,7 +311,6 @@ pub fn AvlTree(
                             last_touched_node = current.node;
                         }
                     } else {
-                        std.debug.print("AvlTree.add(self, gpa, {d}) - creating new node\n", .{val});
                         const new_node = try gpa.create(AvlNode);
                         new_node.* = .{
                             .val = val,
@@ -381,33 +357,24 @@ pub fn AvlTree(
                 var last_touched_node: ?*AvlNode = null;
                 var result: ?T = null;
 
-                std.debug.print("AvlTree.popFront(self, gpa) - adding root to stack\n", .{});
                 try recursion.push(gpa, .{ .traversal_position = TraversalPosition.initial, .node = self.root });
 
                 while (recursion.size > 0) {
-                    std.debug.print("AvlTree.popFront(self, gpa) - popping from stack\n", .{});
                     const current = try recursion.pop(gpa);
                     if (current.node) |node| {
                         if (current.traversal_position == TraversalPosition.initial) {
-                            std.debug.print("AvlTree.popFront(self, gpa) - popped {d} initially\n", .{node.val});
                             if (node.left != null) {
                                 try recursion.push(gpa, .{ .traversal_position = TraversalPosition.fixAvl, .node = current.node });
-                                std.debug.print("AvlTree.popFront(self, gpa) - going left from {d}\n", .{node.val});
                                 try recursion.push(gpa, .{ .traversal_position = TraversalPosition.initial, .node = node.left });
                             } else {
-                                std.debug.print("AvlTree.popFront(self, gpa) - freeing {d}\n", .{node.val});
                                 last_touched_node = node.right;
                                 result = node.val;
                                 node.right = null;
                                 gpa.destroy(node);
                             }
                         } else if (current.traversal_position == TraversalPosition.fixAvl) {
-                            std.debug.print("AvlTree.popFront(self, gpa) - popped {d} to fixAvl\n", .{node.val});
-                            std.debug.print("AvlTree.popFront(self, gpa) - Left pointer value: {*}\n", .{node.left});
-                            std.debug.print("AvlTree.popFront(self, gpa) - last touched value: {*}\n", .{last_touched_node});
                             current.node.?.left = last_touched_node;
                             current.node.?.left = try self.fixAvlProperty(node.left);
-                            std.debug.print("AvlTree.popFront(self, gpa) - Left pointer value: {*}\n", .{node.left});
                             last_touched_node = current.node;
                         }
                     }
